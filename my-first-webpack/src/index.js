22a3aaa3bd712e4b53949047532b9781f70dcaf0
neeleshjaiswal <neelesh.jaiswal08@gmail.com>
2020-10-23 10:57:04 +0530

    feat: added webpack configuration for react app

--- my-first-webpack/src/index.js
+++ my-first-webpack/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-  return <div>Hello {this.props.name}</div>;
+  return <div>Hello {this.props.name}!!!!</div>;
   }
 }
 var mountNode = document.getElementById("app");
